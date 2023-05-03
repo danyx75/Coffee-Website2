1d4912bae8fe065f4f855c14d3fcc4c7b5cf8bd7
danyx75 <danielmartinez053376@gmail.com>
2023-05-03 18:52:34 -0500

    initial commit

--- script.js
+++
@@ -1,6 +0,0 @@
-const togglebutton = document.getElementsByClassName('toggle-button')[0]
-const navlist = document.getElementsByClassName('nav-list')[0]
-
-togglebutton.addEventListener('click', () => {
-  navlist.classList.toggle('active')
-})
